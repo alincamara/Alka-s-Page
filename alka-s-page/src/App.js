deaf96792044e8ee2492c6baadf036c1fbfc8e4e
alincamara9403 <alin.camara@sinoptix.com>
2021-01-27 00:21:56 +0200

    add drag and drop

--- alka-s-page/src/App.js
+++ alka-s-page/src/App.js
@@ -1,13 +1,152 @@
+import { useState } from "react";
 import "./App.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { v4 } from "uuid";
+
+const itemsFromBackend = [
+  { id: v4(), content: "First sound" },
+  { id: v4(), content: "Second sound" },
+  { id: v4(), content: "Third sound" },
+  { id: v4(), content: "Fourth sound" },
+  { id: v4(), content: "Fifth sound" },
+];
+
+const columnsFromBackend = {
+  [v4()]: {
+    name: "Drum Sounds",
+    items: itemsFromBackend,
+  },
+  [v4()]: {
+    name: "Guitar Sounds",
+    items: [],
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+};
 
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
   return (
     <div className="App">
-      <main>
-        <section className="glass"></section>
-      </main>
-      <div className="circle1"></div>
-      <div className="circle2"></div>
+      <div className="glassWrapper">
+        <section className="glass">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              height: "100%",
+            }}
+          >
+            <DragDropContext
+              onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+            >
+              {Object.entries(columns).map(([columnId, column], index) => {
+                return (
+                  <div className="column" key={columnId}>
+                    <div className="columnName">{column.name}</div>
+                    <div style={{ margin: 20 }}>
+                      <Droppable droppableId={columnId} key={columnId}>
+                        {(provided, snapshot) => {
+                          return (
+                            <div
+                              {...provided.droppableProps}
+                              ref={provided.innerRef}
+                              style={{
+                                background: snapshot.isDraggingOver
+                                  ? "#6cdbeb"
+                                  : "#65dfc9",
+                                padding: 10,
+                                width: 200,
+                                height: 400,
+                                overflow: "auto",
+                                borderRadius: 10,
+                              }}
+                            >
+                              {column.items.map((item, index) => {
+                                return (
+                                  <Draggable
+                                    key={item.id}
+                                    draggableId={item.id}
+                                    index={index}
+                                  >
+                                    {(provided, snapshot) => {
+                                      return (
+                                        <div
+                                          ref={provided.innerRef}
+                                          {...provided.draggableProps}
+                                          {...provided.dragHandleProps}
+                                          style={{
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            fontWeight: "bold",
+                                            userSelect: "none",
+                                            padding: 16,
+                                            margin: "0 0 8px 0",
+                                            minHeight: "50px",
+                                            borderRadius: 10,
+                                            backgroundImage: snapshot.isDragging
+                                              ? "linear-gradient(90deg, hsl(0, 98%, 65%) 33%, hsl(17, 98%, 65%) 100%)"
+                                              : "linear-gradient(90deg, hsl(0, 98%, 75%) 33%, hsl(17, 98%, 75%) 100%)",
+                                            color: "white",
+                                            ...provided.draggableProps.style,
+                                          }}
+                                        >
+                                          {item.content}
+                                        </div>
+                                      );
+                                    }}
+                                  </Draggable>
+                                );
+                              })}
+                              {provided.placeholder}
+                            </div>
+                          );
+                        }}
+                      </Droppable>
+                    </div>
+                  </div>
+                );
+              })}
+            </DragDropContext>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
